5588014a488d127734279c2cb5095543971bf5e5
Quinlan Kildare <quinlankildare@gmail.com>
2021-02-14 21:59:19 -0500

    Added Simple landing page

--- project/src/App.js
+++ project/src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
 
 import firebase from 'firebase/app';
 import 'firebase/firestore';
@@ -17,18 +20,10 @@ const firestore = firebase.firestore();
 
 function App() {
 
-    const [user] = useAuthState(auth);
 
     return (
         <div className="App">
-            <header className="App-header">
-                {user ? <LandingPage /> : <SignIn />}
-            </header>
-
-            <section>
-
-            </section>
-
+            {<LandingPage />}
         </div>
     );
 }
@@ -50,8 +45,23 @@ function SignOut() {
 }
 
 function LandingPage() {
+    const [user] = useAuthState(auth);
     return (
-        <section>{<SignOut />}</section>
+        <div className="Landing">
+            {/* PUT NAVBAR HERE PLEASE */}
+            <section className="Landing-about">
+                <div className="Landing-header">What is StockBotics?</div>
+                    <p className="Landing-p">
+                        Stockbotics is a mock trading platform that allows the user to test his/her investment acumen without risking any money. It is an educational tool meant especially for new investors, but can also be used by experienced traders. Using real market data, the user can make simulated trades, allowing him/her to learn the market and track the potential growth of his/her mock portfolio with no risk at all.
+
+                        Unlike other trading platforms such as RobinHood or Fidelity, Stockbotics is totally secure because there's no money and no brokerage involved, so there's no need to give us your sensitive banking information. This also means that there's never any consequences for a bad trade. With Stockbotics, the user is free to make mistakes and take risks, building his/her market knowledge so that when he/she is ready to invest for real, he/she invests with confidence.
+                    </p>
+            </section>
+            <section className="Landing-contact">
+                <div className="Landing-header">Contact Us</div>
+                <a href="https://github.com/ConnorGraham12/POOSD-Project"><FontAwesomeIcon icon={faGithub} /></a>
+            </section>
+        </div>
     )
 }
 
