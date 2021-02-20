6e353a3324ecedad35cc74e3599fb03e6256cca1
kirkcarmichael <kirkacarmichael@gmail.com>
2021-02-20 15:07:37 -0500

    created skeleton for portfolio and generic components for StockEntry and DisplayBox

---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Portfolio.css";
+
+// This should contain a chart comprised of stock entries
+const Portfolio = () => {
+  return (
+    <div>
+      Portfolio
+      <p className="NERD">Some red stuff</p>
+    </div>
+  );
+};
+
+export default Portfolio;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,12 @@
-import React from 'react';
-
+import React from "react";
+import Portfolio from "../components/Portfolio/Portfolio.js";
 const Dashboard = () => {
-	return <div>Dashboard</div>;
+  return (
+    <div>
+      <h1>DashBoard</h1>
+      <Portfolio />
+    </div>
+  );
 };
 
 export default Dashboard;
