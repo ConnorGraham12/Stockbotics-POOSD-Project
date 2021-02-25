a88ababc804bdae635f92dec2f48fda7f541ea9f
kirkcarmichael <kirkacarmichael@gmail.com>
2021-02-25 14:20:45 -0500

    changed StockList to be class based instead of functional

--- src/components/StockEntry/StockList.js
+++ src/components/StockEntry/StockList.js
@@ -1,27 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import "./StockEntry.css";
 import StockEntry from "./StockEntry.js";
 // This should contain a chart comprised of stock entries
 // I think we should make the api call here and manage
 // state in this component
-const StockList = () => {
-  return (
-    <div>
-      This is a StockList. We might have one list for the portfolio, and another
-      for a watchlist.
-      <ul>
-        <li>
-          <StockEntry />
-        </li>
-        <li>
-          <StockEntry />
-        </li>
-        <li>
-          <StockEntry />
-        </li>
-      </ul>
-    </div>
-  );
-};
+class StockList extends Component {
+  render() {
+    return (
+      <div>
+        This is a StockList. We might have one list for the portfolio, and
+        another for a watchlist.
+        <ul>
+          <li>
+            <StockEntry />
+          </li>
+          <li>
+            <StockEntry />
+          </li>
+          <li>
+            <StockEntry />
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default StockList;
