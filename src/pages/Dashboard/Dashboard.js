8e6eb46e07644c66e4c469b3e311eea3cadce286
Quinlan Kildare <quinlankildare@gmail.com>
2021-02-28 10:19:06 -0500

    Add stock API

---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StockList from '../../components/StockEntry/StockList.js';
+const Dashboard = () => {
+	return (
+		<div>
+			<h1>DashBoard</h1>
+			<StockList />
+		</div>
+	);
+};
+
+export default Dashboard;
