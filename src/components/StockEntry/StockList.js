6752f946cd9a0cf541ee21c201aa2cf2c3677c57
Quinlan Kildare <quinlankildare@gmail.com>
2021-03-03 00:25:30 -0500

    Add remove and add assets to dashboard

--- src/components/StockEntry/StockList.js
+++ src/components/StockEntry/StockList.js
@@ -1,98 +1,85 @@
-import React, { Component } from "react";
-import "./StockEntry.css";
-import StockEntry from "./StockEntry.js";
-import { getAssets } from "../../services/firebase";
-import getStockInfo from "../../services/backend";
+import React, { Component, useState, useEffect } from 'react';
+import './StockEntry.css';
+import StockEntry from './StockEntry.js';
+import { getAssets, removeAsset, addAsset } from '../../services/firebase';
+import firebase from '../../services/firebase';
+import getStockInfo from '../../services/backend';
 // This should contain a chart comprised of stock entries
 // I think we should make the api call here and manage
 // state in this component
 
-class StockList extends Component {
-  // this is the state of the StockList
-  state = { stocks: [] };
+function StockList() {
+	// this is the state of the StockList
+	const [stocks, setStocks] = useState([]);
+	const [searchSymbol, setSearchSymbol] = useState('');
+	const [addedShares, setAddedShares] = useState(0);
 
-  componentWillMount() {
-    const updateState = async () => {
-      // getAssets returns an array of objects
-      let assets = await getAssets();
+	useEffect(() => {
+		firebase.auth().onAuthStateChanged(() => {
+			getAssets().then((assets) => {
+				setStocks(assets);
+			});
+		});
+	}, []);
 
-      // create a local copy of the current state stock list
-      let stateCopy = [...this.state.stocks];
+	// remove stocks from portfolio
+	const removeStockHandler = async (event, stockID) => {
+		// create a copy of the current stock array in state (don't mutate the state)
+		const stateStocksCopy = [...stocks];
 
-      // we run into issues if there are no stocks for the user
-      if (assets == null || assets.length == 0) {
-        stateCopy = [];
-        stateCopy[0] = {
-          symbol: "hardcoded placeholder",
-        };
-      } else {
-        stateCopy = assets;
-      }
+		// get the index of the stock we want to delete
+		const indexOfTarget = stateStocksCopy.findIndex((curStock) => {
+			return curStock.stockID == stockID;
+		});
 
-      this.setState({
-        stocks: stateCopy,
-      });
-    };
+		// remove the stock at that index
+		stateStocksCopy.splice(indexOfTarget, 1);
+		removeAsset(stateStocksCopy);
+		setStocks(stateStocksCopy);
+	};
 
-    updateState();
-  }
+	// add stocks to the portfolio
+	const addStockHandler = () => {
+		let stockCopy = [...stocks];
+		stockCopy.push({ symbol: searchSymbol, shares: parseInt(addedShares) });
+		addAsset(stocks, searchSymbol, addedShares);
+		setStocks(stockCopy);
+	};
+	// array of JSX objects (one for each stock)
+	let allStocks = null;
+	const showStocks = () => {
+		return !stocks ? (
+			<div>You don't have any stocks... maybe you should add one...</div>
+		) : (
+			<div>
+				{stocks.map((curStock) => {
+					return (
+						<StockEntry
+							key={curStock.symbol}
+							symbol={curStock.symbol}
+							shares={curStock.shares}
+							value={curStock.value}
+							oneDayReturn={curStock.oneDayReturn}
+							overallReturn={curStock.overallReturn}
+							returnWithSells={curStock.returnWithSells}
+							pricePerShare={curStock.pricePerShare}
+							remove={(event) => removeStockHandler(event, curStock.symbol)}
+						/>
+					);
+				})}
+			</div>
+		);
+	};
 
-  // remove stocks from portfolio
-  removeStockHandler = async (event, stockID) => {
-    // create a copy of the current stock array in state (don't mutate the state)
-    const stateStocksCopy = [...this.state.stocks];
-
-    // get the index of the stock we want to delete
-    const indexOfTarget = stateStocksCopy.findIndex((curStock) => {
-      return curStock.stockID == stockID;
-    });
-
-    // remove the stock at that index
-    stateStocksCopy.splice(indexOfTarget, 1);
-
-    // update the state to reflect the removal
-    this.setState({
-      stocks: stateStocksCopy,
-    });
-  };
-
-  // add stocks to the portfolio
-  addStockHandler = (event, stockID) => {};
-  render() {
-    // array of JSX objects (one for each stock)
-    let allStocks = null;
-    const showStocks = (
-      <div>
-        {this.state.stocks.map((curStock) => {
-          return (
-            <StockEntry
-              key={curStock.symbol}
-              symbol={curStock.symbol}
-              shares={curStock.shares}
-              value={curStock.value}
-              oneDayReturn={curStock.oneDayReturn}
-              overallReturn={curStock.overallReturn}
-              returnWithSells={curStock.returnWithSells}
-              pricePerShare={curStock.pricePerShare}
-              remove={(event) =>
-                this.removeStockHandler(event, curStock.symbol)
-              }
-            />
-          );
-        })}
-      </div>
-    );
-
-    return (
-      <div>
-        <button>add stock (not working rn)</button>
-        <input type="text" value="enter stock symbol"></input>
-        This is a StockList. We might have one list for the portfolio, and
-        another for a watchlist.
-        {showStocks}
-      </div>
-    );
-  }
+	return (
+		<div>
+			<button onClick={addStockHandler}>add stock</button>
+			<input type='text' placeholder='stock symbol' onInput={(e) => setSearchSymbol(e.target.value)}></input>
+			<input type='number' placeholder='number of shares' onInput={(e) => setAddedShares(e.target.value)}></input>
+			{/* This is a StockList. We might have one list for the portfolio, and another for a watchlist. */}
+			{showStocks()}
+		</div>
+	);
 }
 
 export default StockList;
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -6,12 +6,44 @@ const config = require('../config.json');
 
 firebase.initializeApp(config.FIREBASE);
 
+// For development only
+// firebase.firestore().useEmulator('localhost', 8080);
+// firebase.auth().useEmulator('http://localhost:9099');
+
 export async function getAssets() {
 	const database = firebase.firestore();
 	const auth = firebase.auth();
+
 	if (!auth.currentUser) return;
 	const user = await database.collection('users').doc(auth.currentUser.uid).get();
 	return user.data().assets;
+}
+
+export async function removeAsset(assets) {
+	const database = firebase.firestore();
+	const auth = firebase.auth();
+
+	if (!auth.currentUser) return;
+	const user = database.collection('users').doc(auth.currentUser.uid);
+	return user.update({ assets: assets });
+}
+
+export function addAsset(assets, symbol, shares) {
+	const database = firebase.firestore();
+	const auth = firebase.auth();
+	const indexOfTarget = assets.findIndex((curStock) => {
+		return curStock.symbol == symbol;
+	});
+	if (indexOfTarget == -1) assets.push({ symbol: symbol, shares: parseInt(shares) });
+	else {
+		var tempShares = parseInt(assets[indexOfTarget].shares);
+		tempShares += parseInt(shares);
+		assets[indexOfTarget].shares = parseInt(tempShares);
+	}
+
+	if (!auth.currentUser) return;
+	const user = database.collection('users').doc(auth.currentUser.uid);
+	return user.update({ assets: assets });
 }
 
 export function CreateUser() {
