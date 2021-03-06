bacd7a24534375e7f3210667e08ae7f67df0894a
jrm5399 <44210814+jrm5399@users.noreply.github.com>
2021-03-05 22:41:30 -0500

    Updating the time needed for all commands to run

--- win_start.bat
+++ win_start.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 start "Windows PowerShell" cmd /c "npm start"
 start "Windows PowerShell" cmd /c "firebase emulators:start"
-timeout 10 > nul
+timeout 12 > nul
 start "Windows PowerShell" cmd /c "cd react-backend && SET PORT = 3001 && node bin/www"
 PAUSE
 taskkill  /FI "WINDOWTITLE eq Windows PowerShell"
