cfa6a272fcc940b9791a5f206474612e282c726b
kirkcarmichael <kirkacarmichael@gmail.com>
2021-03-23 17:54:00 -0400

    added StockChart component

---
+++ src/components/StockChart/StockChart.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StockChart = () => {
+  return <div></div>;
+};
+
+export default StockChart;
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,27 +1,30 @@
-import React, { useState } from 'react';
-import './Dashboard.css';
-import StockList from '../../components/StockEntry/StockList.js';
+import React, { useState } from "react";
+import "./Dashboard.css";
+import StockList from "../../components/StockEntry/StockList.js";
 const Dashboard = () => {
-	const [accountValue, setAccountValue] = useState(0);
+  const [accountValue, setAccountValue] = useState(0);
 
-	const updateAccountValue = (value) => {
-		setAccountValue(value + accountValue);
-	};
+  const updateAccountValue = (value) => {
+    setAccountValue(value + accountValue);
+  };
 
-	return (
-		<div>
-			<div className='DashboardItems'>
-				<StockList className='Asset-List' updateAccountValue={updateAccountValue} />
-				<div className='Profile-Info'>
-					<div className='Chart'>A BIG OL CHART BABY</div>
-					<div className='Data'>
-						<p>This is where data will go about value, etc.</p>
-						<p>Account Value: {accountValue}</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="DashboardItems">
+        <StockList
+          className="Asset-List"
+          updateAccountValue={updateAccountValue}
+        />
+        <div className="Profile-Info">
+          <div className="Chart"></div>
+          <div className="Data">
+            <p>This is where data will go about value, etc.</p>
+            <p>Account Value: {accountValue}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
